eb09d4f87cd994febc3a965a32cb8bf9d01a5cd4
Richard Kong <richard@pharmadata.net.au>
2015-09-17 10:04:32 +1000

    updated sort and print summary of users retrieved vs expected

--- intercom_duplicates.py
+++ intercom_duplicates.py
@@ -2,13 +2,14 @@
 import requests
 
 intercom_api_key = 'API_KEY'
-intercom_app_id = 'API_ID'
+intercom_app_id = 'APP_ID'
 start_page = 1
-end_page = 20
+end_page = 62
 
 users = {}
+emails = []
 for i in range(start_page,end_page):
-    payload = {'page': i, 'per_page': 50}
+    payload = {'page': i, 'per_page': 50, 'sort':'created_at','order':'asc'}
     headers = {'Accept': 'application/json'}    
     r = requests.get('https://api.intercom.io/users', 
                      headers = headers,
@@ -22,8 +23,19 @@ for i in range(start_page,end_page):
         else:
             users[user["id"]] = [i]
             
+        emails.append(user["email"])
             
 for user in users:
      if len(users[user]) > 1:
          print "id = {0} pages = {1} ".format(user,str(users[user] ))
+
+print "expected total users = {0}".format(response["total_count"])
+print "unique emails = {0}".format(len(set(emails)))
+print "unique ids = {0}".format(len(users))
+print "total emails = {0}".format(len(emails))
+
+
+
+
+    
 
